9670760685d003e4d415ee3dc8005a4d9cb6fa6e
Joseph Nguyen <144830286+jjjosephn@users.noreply.github.com>
2024-01-31 21:33:47 -0800

    Create hw1.py

---
+++ HW/hw1.py
@@ -0,0 +1,19 @@
+def palindrome(s):
+    i = 0
+    j = len(s) - 1
+
+    while i < j:
+        if not s[i].isalnum():
+            i += 1
+            continue
+        if not s[j].isalnum():
+            j -= 1
+            continue
+        if s[i].lower() != s[j].lower():
+            return False
+        if s[i].lower() != s[j].lower():
+            i += 1
+            j -= 1
+        return True
+    
+print(palindrome("M$ad-Am"))
