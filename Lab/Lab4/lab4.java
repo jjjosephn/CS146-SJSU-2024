4a2d7422e2bcafd954ef5577a785225318be5d1a
Joseph Nguyen <144830286+jjjosephn@users.noreply.github.com>
2024-03-07 23:55:24 -0800

    Create lab4.java

---
+++ Lab/Lab4/lab4.java
@@ -0,0 +1,12 @@
+public class lab4{
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.right);
+        invertTree(root.left);
+        return root;
+    }
+}
